269a0e9a2ddbc6db7665f3f2dd44ae881edd958c
ADAM STONE <astone13@students.towson.edu>
2017-04-20 14:54:20 -0400

    remove TODO comment

--- WMethod/WMethod.java
+++ WMethod/WMethod.java
@@ -381,12 +381,6 @@ public class WMethod{
      Vector <String> tests=generateTests(transitionCover, w); // Generate tests.
      Utilities.printAllTestCases(tests); // Print tests.
      
-     // TODO: 	Write the necessary code to iterate through all test cases and run them against
-     // 		the FSM using the Utilities.runFSM() method. 
-     //
-     // Example use of the Utilities.runFSM() method
-     // Utilities.runFSM(FSM, 1, "a a b a b", " ");
-     
      Iterator testIterator = tests.iterator();
      
      while(testIterator.hasNext()) {
